110606961a207b0421b84789916fb3a11150b096
johnmogi <anguru@gmail.com>
2022-02-09 17:35:39 +0700

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String x = "Hello world";
+            bool b = false;
+            int i = 1220;
+
+            Console.WriteLine(x);
+        }
+    }
+}
